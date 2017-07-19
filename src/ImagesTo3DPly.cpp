0043d3d024ff443ffa65503c074ccb765431ed3e
AaronWr <aaron.wreschino@smail.th-koeln.de>
2017-07-19 21:59:49 +0200

    the programm now dont uses fixed path for images

--- src/ImagesTo3DPly.cpp
+++ src/ImagesTo3DPly.cpp
@@ -1,81 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 
 // Scrupt Dir
-char openMVG_path[] = "../openMVG_Build/Linux-x86_64-RELEASE/";
-char openMVE_path[] = "../mve/apps/";
+std::string openMVG_path = "../openMVG_Build/Linux-x86_64-RELEASE/";
+std::string openMVE_path = "../mve/apps/";
+std::string res_path = "";
+ 
+int main(int argc, char *argv[])
+{
+	if(argc != 2){
+		std::cout << "Wrong arguments" << "\n";
+		return 0;
+	}
+	
+	res_path = argv[1];
+	
 
-int main (int ac, char** av) {
-	
 	// Image Listing
-	char ch[255] = "";
-	strcat(ch, openMVG_path);
-	strcat(ch, "openMVG_main_SfMInit_ImageListing");
-	strcat(ch, " -d ../openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt");
-	strcat(ch, " -i ../res/images");
-	strcat(ch, " -o ../res/output");
-	system(ch);
-	
+	std::string ch = openMVG_path + "openMVG_main_SfMInit_ImageListing -d ../openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt -i " + res_path + " -o " + res_path + "output";
+	system(ch.c_str());
+
 	// Compute Features
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVG_path);
-	strcat(ch, "openMVG_main_ComputeFeatures");
-	strcat(ch, " -i ../res/output/sfm_data.json");
-	strcat(ch, " -o ../res/output/image");
-	strcat(ch, " -n 4");
-	system(ch);
+	ch.clear();
+	ch = openMVG_path + "openMVG_main_ComputeFeatures -i " + res_path + "output/sfm_data.json -o " + res_path + "output/image -n 4";
+	system(ch.c_str());
 	
 	// Compute Matches
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVG_path);
-	strcat(ch, "openMVG_main_ComputeMatches");
-	strcat(ch, " -i ../res/output/sfm_data.json");
-	strcat(ch, " -o ../res/output/image");
-	system(ch);
+	ch.clear();
+	ch = openMVG_path + "openMVG_main_ComputeMatches -i " + res_path + "output/sfm_data.json -o " + res_path + "output/image";
+	system(ch.c_str());
 
 	// IncrementalSfM
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVG_path);
-	strcat(ch, "openMVG_main_IncrementalSfM");
-	strcat(ch, " -i ../res/output/sfm_data.json");
-	strcat(ch, " -m ../res/output/image");
-	strcat(ch, " -o ../res/output/IncrementalRecon");
-	system(ch);
+	ch.clear();
+	ch = openMVG_path + "openMVG_main_IncrementalSfM -i " + res_path + "output/sfm_data.json -m " + res_path + "output/image -o " + res_path + "output/IncrementalRecon";
+	system(ch.c_str());
 
 	// Convert scene to MVE format
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVG_path);
-	strcat(ch, "openMVG_main_openMVG2MVE2");
-	strcat(ch, " -i ../res/output/IncrementalRecon/sfm_data.bin");
-	strcat(ch, " -o ../res/output/OutRecon");
-	system(ch);
-	
-	// Depth Map reconstruction
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVE_path);
-	strcat(ch, "dmrecon/dmrecon");
-	strcat(ch, " -s2 ../res/output/OutRecon/MVE/");
-	system(ch);
-	
-	// Scene to Point Set
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVE_path);
-	strcat(ch, "scene2pset/scene2pset");
-	strcat(ch, " -F2 ../res/output/OutRecon/MVE/ ../res/output/OutRecon/MVE/pset-L2.ply");
-	system(ch);
+	ch.clear();
+	ch = openMVG_path + "openMVG_main_openMVG2MVE2 -i " + res_path + "output/IncrementalRecon/sfm_data.bin -o " + res_path + "output/OutRecon";
+	system(ch.c_str());
 
+	// Depth Map reconstruction
+	ch.clear();
+	ch = openMVE_path + "dmrecon/dmrecon -s2 " + res_path + "output/OutRecon/MVE/";
+	system(ch.c_str());
+	// Scene to Point Set
+	ch.clear();
+	ch = openMVE_path + "scene2pset/scene2pset -F2 " + res_path + "output/OutRecon/MVE/ " + res_path + "output/OutRecon/MVE/pset-L2.ply";
+	system(ch.c_str());
 	// Meshing
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVE_path);
-	strcat(ch, "fssrecon/fssrecon");
-	strcat(ch, " ../res/output/OutRecon/MVE/pset-L2.ply ../res/output/OutRecon/surface-L2.ply");
-	system(ch);
+	ch.clear();
+	ch = openMVE_path + "fssrecon/fssrecon " + res_path + "output/OutRecon/MVE/pset-L2.ply " + res_path + "output/OutRecon/surface-L2.ply";
+	system(ch.c_str());
 
 	// Meshing Cleaning
-	memset(ch, 0, sizeof ch);
-	strcat(ch, openMVE_path);
-	strcat(ch, "meshclean/meshclean");
-	strcat(ch, " -t10 ../res/output/OutRecon/surface-L2.ply ../res/output/OutRecon/surface-L2-clean.ply");
-	system(ch);
+	ch.clear();
+	ch = openMVE_path + 
+	ch = "meshclean/meshclean -t10 " + res_path + "output/OutRecon/surface-L2.ply " + res_path + "output/OutRecon/surface-L2-clean.ply";
+	system(ch.c_str());
 }
